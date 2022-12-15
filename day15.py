cb64ad9c8ee6c26776a1c1c8eef4832799d838c0
nauqs <aquindsa10@alumnes.ub.edu>
2022-12-15 18:30:31 +0000

    Comments for day 15

--- day15.py
+++ day15.py
@@ -11,30 +11,35 @@ def parse_input(data):
     return closest 
 
 def combine(ranges, x_min=-999999999, x_max=999999999):
-  ranges = sorted(ranges, key=lambda x: x[0])
-  result = [ranges[0]]
-  for start, end in ranges[1:]:
-    last_start, last_end = result[-1]
-    if start <= last_end + 1: result[-1] = (last_start, max(last_end, end))
-    else: result.append((start, end))
+    # combine a list of ranges in a sorted, non-overlapping list (GPT helped haha)
+    ranges = sorted(ranges, key=lambda x: x[0])
+    result = [ranges[0]]
+    for start, end in ranges[1:]:
+        last_start, last_end = result[-1]
+        if start <= last_end + 1: result[-1] = (last_start, max(last_end, end))
+        else: result.append((start, end))
 
-  s = 0
-  for rng in result:
-    s += np.clip(rng[1], x_min, x_max)-np.clip(rng[0], x_min, x_max)
+    s = 0
+    for rng in result:
+        s += np.clip(rng[1], x_min, x_max)-np.clip(rng[0], x_min, x_max)
 
-  return result, s
+    return result, s
 
 
 def part1(closest, row=10):
     d = {}
     for s in closest:
+        # for each sensor we store the size of the square it covers
         d[s] = np.sum(np.abs(np.array(s)-closest[s]))
 
     covered = []
     for s in d:
         s_pos = np.array(s)
         dist = np.abs(s_pos[1]-row)
+        # for each sensor, we compute how far it is from the interest row
+        # if the sensor covers the row, we compute which interval it covers
         if dist<d[s]:
+            # => it covers an interval of range 2*(square_side-distance)
             covered.append((s_pos[0]-(d[s]-dist), s_pos[0]+(d[s]-dist)))
 
     return combine(covered)[1]
@@ -44,6 +49,7 @@ def part2(closest, y_min=0, y_max=20):
     for s in closest:
         d[s] = np.sum(np.abs(np.array(s)-closest[s]))
 
+    # repeat part 1 for each distance
     for row in tqdm.tqdm(range(y_min,y_max)):
         covered = []
         for s in d:
@@ -52,8 +58,11 @@ def part2(closest, y_min=0, y_max=20):
             if dist<d[s]:
                 covered.append((s_pos[0]-(d[s]-dist), s_pos[0]+(d[s]-dist)))
         res, s = combine(covered, x_min=y_min, x_max=y_max)
+        # if the covered interval s is not exactly y_max-y_min WE FOUND IT!
         if y_max-y_min!=s:
-            return 4000000*(res[0][1]+1)+row
+            # res = [(y_min, y_beacon-1), (y_beacon+1, y_max)]
+            y_beacon = (res[0][1]+1)
+            return y_beacon*4000000 + row
 
 if __name__ == "__main__":
     day = 15
